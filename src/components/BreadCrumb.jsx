610616631be6ef598836ae48969b0b640d70b4de
Alfian Ramadani <alfianr792@gmail.com>
2024-08-29 13:21:05 -0400

    feat: Add BreadCrumb component for displaying page navigation

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function BreadCrumb({ title ,pageNow }) {
+    return (
+        <section className=" bg-accent w-full text-background ">
+            <div className="max-w-7xl h-40 mx-auto flex flex-col justify-center gap-2">
+                <h1 className="text-5xl italic font-semibold  tracking-wide">{title}</h1>
+                <ul className="flex gap-3 text-md">
+                    <li>
+                        <Link className="underline" to="/">Home</Link>
+                    </li>
+                    <li>
+                        /
+                    </li>
+                    <li>
+                        {pageNow}
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
