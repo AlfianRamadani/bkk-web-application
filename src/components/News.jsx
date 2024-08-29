746df630a54d145cd44e5f3e6bee9af04fcca85a
Alfian Ramadani <alfianr792@gmail.com>
2024-08-29 13:21:56 -0400

    feat: Add News component for displaying news articles

---
+++ src/components/News.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function News() {
+  return (
+    <article className="flex flex-nowrap overflow-hidden border-b-[1px] py-3 mx-auto border-primary-text group">
+    <div className="flex flex-nowrap gap-20 ">
+      <div className="flex whitespace-nowrap gap-6 animate-startRight group-hover:animate-pause px-10">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div className="flex whitespace-nowrap gap-6 animate-startRight group-hover:animate-pause">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+    </div>
+  </article>
+  )
+}
